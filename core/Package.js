4d1cc323836f06d35f70bb6f6d63638230f977b4
GanapathiMangipudi <57028826+GanapathiMangipudi@users.noreply.github.com>
2025-11-24 05:13:29 +0530

    Add files via upload

---
+++ core/Package.js
@@ -0,0 +1,8 @@
+export class Package{
+    constructor(tracking_id,size,destination)
+    {
+        this.tracking_id=tracking_id;
+        this.size=size;
+        this.destination=destination;
+    }
+}
