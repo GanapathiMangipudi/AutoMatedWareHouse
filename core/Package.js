7255e5efb6f4613c07bdf950c8b152ebb7dc6e0e
GanapathiMangipudi <57028826+GanapathiMangipudi@users.noreply.github.com>
2025-11-24 11:09:11 +0530

    Delete core directory

--- core/Package.js
+++
@@ -1,8 +0,0 @@
-export class Package{
-    constructor(tracking_id,size,destination)
-    {
-        this.tracking_id=tracking_id;
-        this.size=size;
-        this.destination=destination;
-    }
-}
